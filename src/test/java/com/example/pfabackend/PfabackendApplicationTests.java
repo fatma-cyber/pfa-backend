3a433c2042f7053c56d5f886aa1209eff60ab762
fatma-cyber <fatmaezzahra.bouachir@fsb.ucar.tn>
2025-04-09 09:05:52 +0100

    Initial commit - setup Spring Boot project

---
+++ src/test/java/com/example/pfabackend/PfabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pfabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
